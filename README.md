3076a0c0c3518ab62325cd10ba2372676b12d733
Ayme <amorr037@fiu.edu>
2016-02-16 20:41:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dutings-server
+[![Build Status](https://travis-ci.org/amorr037/dutings-server.svg?branch=master)](https://travis-ci.org/amorr037/dutings-server)
---
+++ tests/ApiTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: javier
+ * Date: 2/15/16
+ * Time: 11:34 PM
+ */
+class ApiTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testAlwaysPass()
+    {
+        $this->assertEquals(0, 0);
+    }
+}
